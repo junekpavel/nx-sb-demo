d7d470f8f33cdb21de5941d1f557782f1f6cf5cf
Pavel Junek <junek.pavel.pj@gmail.com>
2021-11-02 09:45:23 +0100

    feat(SecondLib): generate stories

---
+++ packages/second-lib/src/lib/demo/demo.component.stories.ts
@@ -0,0 +1,22 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { DemoComponent } from './demo.component';
+
+export default {
+  title: 'DemoComponent',
+  component: DemoComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    })
+  ],
+} as Meta<DemoComponent>;
+
+const Template: Story<DemoComponent> = (args: DemoComponent) => ({
+  component: DemoComponent,
+  props: args,
+});
+
+
+export const Primary = Template.bind({});
+Primary.args = {
+}
